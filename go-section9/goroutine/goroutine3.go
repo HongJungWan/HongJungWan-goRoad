59bcc1d7b0f20c57c4f9ed7f58096fc41e3fc573
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-06-09 21:30:16 +0900

    feat: Go 병행처리
    1. 고루틴

--- go-section9/goroutine/goroutine3.go
+++ go-section9/goroutine/goroutine3.go
@@ -1,4 +1,4 @@
-// 고루틴(Goroutine)기초(3)
+// 고루틴(Goroutine) 기초 (3)
 package main
 
 import "fmt"
@@ -9,6 +9,7 @@ import "runtime"
 func exe(name int) {
 	r := rand.Intn(100)
 	fmt.Println(name, " start : ", time.Now())
+
 	for i := 0; i < 100; i++ {
 		fmt.Println(name, ">>>>>>>", r, i)
 	}
@@ -16,17 +17,17 @@ func exe(name int) {
 }
 
 func main() {
-	//고루틴(Goroutine)
-	//멀티 코어(다중 CPU) 최대한 활용
+	// 고루틴(Goroutine)
 
+	// 멀티 코어(다중 CPU) 최대한 활용
 	runtime.GOMAXPROCS(runtime.NumCPU())                        // 현 시스템의 CPU 코어 개수 반환 후 설정
 	fmt.Println("Current System Cpu : ", runtime.GOMAXPROCS(0)) // 설정 값 출력
 
-	//예제1
+	// 예제 1
 	fmt.Println("Main Routine Start : ", time.Now())
 	for i := 0; i < 100; i++ {
-		go exe(i) //고루틴 100개 생성
+		go exe(i) // 고루틴 100개 생성
 	}
-	time.Sleep(5 * time.Second) //time.Second, Minute, Hour, Millisecond ....
+	time.Sleep(5 * time.Second)
 	fmt.Println("Main Routine End : ", time.Now())
 }
